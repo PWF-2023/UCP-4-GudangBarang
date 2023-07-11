2c9ebb3ca875b2ff6bd7e6d69060e5348c42c401
Naufal Rozan <naufal.rozan.ft20@mail.umy.ac.id>
2023-07-11 20:20:36 +0700

    Menambahkan UserController, Middleware & Gate, Mailtrap

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,9 +21,12 @@
                     <x-nav-link :href="route('category.index')" :active="request()->routeIs('category')">
                         {{ __('Category') }}
                     </x-nav-link>
-                    {{-- <x-nav-link :href="route('user.index')" :active="request()->routeIs('user')">
+                    @can('admin')
+                    <x-nav-link :href="route('user.index')" :active="request()->routeIs('user')">
                         {{ __('User') }}
-                    </x-nav-link> --}}
+                    </x-nav-link>
+                    @endcan
+
                 </div>
             </div>
 
@@ -85,9 +88,11 @@
             <x-responsive-nav-link :href="route('category.index')" :active="request()->routeIs('category')">
                 {{ __('Category') }}
             </x-responsive-nav-link>
-            {{-- <x-responsive-nav-link :href="route('user.index')" :active="request()->routeIs('user')">
+            @can('admin')
+            <x-responsive-nav-link :href="route('user.index')" :active="request()->routeIs('user')">
                 {{ __('User') }}
-            </x-responsive-nav-link> --}}
+            </x-responsive-nav-link>
+            @endcan
         </div>
 
         <!-- Responsive Settings Options -->
